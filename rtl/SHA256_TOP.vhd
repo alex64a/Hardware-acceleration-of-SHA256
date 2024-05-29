be61ec5fc27f71e48ed6657671a5557aa04c4088
Aleksa <50616697+alex64a@users.noreply.github.com>
2024-05-29 23:00:39 +0200

    Add files via upload

---
+++ rtl/SHA256_TOP.vhd
@@ -0,0 +1,66 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity SHA256_TOP is
+    Port (
+        clk : in std_logic;
+        reset : in std_logic;
+        start : in std_logic;
+        N_i : in std_logic_vector(31 downto 0);
+        M_i : in std_logic_vector(31 downto 0);
+        H : out std_logic_vector(255 downto 0);
+        ready : out std_logic
+    );
+end SHA256_TOP;
+
+architecture rtl of SHA256_TOP is
+
+    -- Signals for interconnecting datapath and controlpath
+    signal i_lt_N_s     : std_logic;
+    signal w_finished_s : std_logic;
+    signal load_m_s     : std_logic;
+    signal state_s      : std_logic_vector(2 downto 0);
+    signal compute_t_s  : std_logic;
+    signal N_take_in_s  : std_logic;
+    signal t_lt_64_s    : std_logic;
+
+begin
+
+    -- Instantiate datapath
+    u_datapath: entity work.datapath
+        port map (
+            clk           => clk,
+            reset         => reset,
+            N_i           => N_i,
+            M_i           => M_i,
+            i_lt_N_o      => i_lt_N_s,
+            w_finished_o  => w_finished_s,
+            load_m_i      => load_m_s,
+            state_i       => state_s,
+            compute_t_i   => compute_t_s,
+            N_take_in_i   => N_take_in_s,
+            t_lt_64_o     => t_lt_64_s,
+            H             => H
+        );
+
+    -- Instantiate controlpath
+    u_controlpath: entity work.controlpath
+        port map (
+            clk           => clk,
+            reset         => reset,
+            start         => start,
+            i_lt_N_i      => i_lt_N_s,
+            w_finished_i  => w_finished_s,
+            load_m_o      => load_m_s,
+            state_o       => state_s,
+            t_lt_64_i     => t_lt_64_s,
+            compute_t_o   => compute_t_s,
+            N_take_in_o   => N_take_in_s,
+            ready         => ready
+        );
+
+end architecture rtl;
--- rtl/controlpath.vhd
+++ rtl/controlpath.vhd
@@ -15,13 +15,14 @@ entity controlpath is
         state_o  : out std_logic_vector(2 downto 0);
         t_lt_64_i   : in std_logic;
         compute_t_o : out std_logic;
+        N_take_in_o : out std_logic;
         
         ready : out std_logic
     );
 end controlpath;
 
 architecture Behavioral of controlpath is
-    type state_type is (IDLE, LOAD_W, W_LOADED, SIG, T_PHASE, COMPUTE_V, UPDATE_H);
+    type state_type is (IDLE, LOAD_W, W_LOADING, SIG, T_PHASE, COMPUTE_V, UPDATE_H);
     signal state_reg, state_next : state_type;
 begin
 
@@ -31,36 +32,43 @@ begin
             state_reg <= IDLE;
         elsif rising_edge(clk) then
             state_reg <= state_next;
-        end process;
+        end if;
+    end process;
      
-     next_state_logic:process(state_reg, start )
+     next_state_logic:process(state_reg, start, w_finished_i, i_lt_N_i, t_lt_64_i )
      begin
+          N_take_in_o <= '0';
+          ready <= '0';
+          load_m_o <= '0';
+          compute_t_o <= '0';
          case state_reg is
              when IDLE =>
                  ready <= '1';
                  state_o    <= "000";
                  if start = '1' then
                      state_next <= LOAD_W;
+                     N_take_in_o <= '1';
                  else
                      state_next <= IDLE;
+                     N_take_in_o <= '0';
                  end if;
      
              when LOAD_W =>
                   state_o    <= "001";
                  if i_lt_N_i = '1' then
                      load_m_o <= '1';
-                     state_next <= W_LOADED;
+                     state_next <= W_LOADING;
                  else
                      load_m_o <= '0';
                      state_next <= IDLE;
                  end if;
                  
-              when W_LOADED =>
+              when W_LOADING =>
                   state_o    <= "010";
                  if w_finished_i = '1' then
                      state_next <= SIG;
                  else
-                     state_next <= W_LOADED;
+                     state_next <= W_LOADING;
                  end if;
      
              when SIG =>
@@ -70,7 +78,7 @@ begin
              when T_PHASE =>
                    state_o    <= "100";
                  if t_lt_64_i = '1' then
-                    compute_t_o <= '1';
+                     compute_t_o <= '1';
                      state_next <= COMPUTE_V;
                  else
                      compute_t_o <= '0';
@@ -79,7 +87,7 @@ begin
      
              when COMPUTE_V =>
                  state_o    <= "101";
-                 state_next <= COMPUTE_V;
+                 state_next <= T_PHASE;
      
              when UPDATE_H =>
                 state_o    <= "110";
