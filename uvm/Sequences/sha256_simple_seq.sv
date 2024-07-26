922a6abbac59cd68caacd585a0ed74249ec6fb67
Aleksa Mitrovčan <50616697+alex64a@users.noreply.github.com>
2024-07-26 17:46:21 +0200

    Add files via upload

--- uvm/Sequences/sha256_simple_seq.sv
+++ uvm/Sequences/sha256_simple_seq.sv
@@ -9,9 +9,40 @@ class sha256_simple_seq extends sha256_base_seq;
       super.new(name);
    endfunction
 
+   logic test_string[16] = {
+      32'h31800000',
+      32'h00000000',
+      32'h00000000',
+      32'h00000000',
+      32'h00000000',
+      32'h00000000',
+      32'h00000000',
+      32'h00000000',
+      32'h00000000',
+      32'h00000000',
+      32'h00000000',
+      32'h00000000',
+      32'h00000000',
+      32'h00000000',
+      32'h00000000',
+      32'h00000008',
+   }
+
    virtual task body();
-      // simple example - just send one item
-      `uvm_do(req);
+      
+  `uvm_do_with(req, {  req.start == 1; 
+                       req.N_i == 32'h00000001';})
+
+  `uvm_info("sha256_simple_seq", "Block count sent", UVM_LOW);
+
+  for(int i = 0; i < 16; i++)
+  {
+   `uvm_do_with(req, {req.M_i == test_string[i];} )
+  } 
+  `uvm_info("sha3_simple_seq", "Padded message sent", UVM_LOW);
+
+
+
    endtask : body
 
 endclass : sha256_simple_seq
